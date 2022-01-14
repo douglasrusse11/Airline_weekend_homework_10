dbfa68fe3477baf35c485401f21cc55222b7fed6
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-14 14:22:54 +0000

    adds test for Passenger

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,6 +1,15 @@
+import flight.Flight;
 import person.Passenger;
 import org.junit.Before;
 import org.junit.Test;
+import person.airlineStaff.CabinCrewMember;
+import person.airlineStaff.Pilot;
+import person.airlineStaff.Rank;
+import plane.Plane;
+import plane.PlaneType;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,5 +30,18 @@ public class PassengerTest {
     @Test
     public void hasNumberOfBags() {
         assertEquals(2, passenger.getNumberOfBags());
+    }
+
+    @Test canAddFlight() {
+        ArrayList<Pilot> pilots = new ArrayList<>();
+        pilots.add(new Pilot("Bob", Rank.FIRST_OFFICER, "ABCD123"));
+        pilots.add(new Pilot("Ted", Rank.CAPTAIN, "ABCD123"));
+        CabinCrewMember[] cabinCrewMembers = new CabinCrewMember[2];
+        cabinCrewMembers[0] = new CabinCrewMember("Steve", Rank.FLIGHT_ATTENDANT);
+        cabinCrewMembers[1] = new CabinCrewMember("John", Rank.PURSER);
+        Plane plane = new Plane(PlaneType.BOEING747, 467, 124000);
+        Flight flight = new Flight(pilots, cabinCrewMembers, plane, "FR756", "EDI", "GLA", new Date(2022, 1, 14, 13, 0));
+        passenger.addFlight(flight);
+        assertEquals(flight, passenger.getFlight());
     }
 }
