d2e5db4c576a99918728fc1adebd5997c1d8a452
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-14 11:19:45 +0000

    adds tests for Plane

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import plane.Plane;
+import plane.PlaneType;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaneTest {
+
+    private Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING747, 467, 124000);
+    }
+
+    @Test
+    public void hasPlaneType() {
+        assertEquals(PlaneType.BOEING747, plane.getPlaneType());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(467, plane.getCapacity());
+    }
+
+    @Test
+    public void hasTotalWeight() {
+        assertEquals(124000, plane.getTotalWeight, 0.0);
+    }
+}
